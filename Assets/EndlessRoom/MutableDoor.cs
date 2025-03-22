e6f4789023777538ea54ff5050a4b31016f016c6
Victor Liu <victorliu1231@gmail.com>
2025-03-22 18:12:14 -0400

    prelim door creak

--- Assets/EndlessRoom/MutableDoor.cs
+++ Assets/EndlessRoom/MutableDoor.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class MutableDoor : MonoBehaviour {
     public float playerDistToDoorTriggerCreak;
     public float playerDistToDoorTriggerKill;
+    public Animator doorAnimator;
+    public string doorCreakAnimationName;
+    public string doorOpenAnimationName;
+    public AudioSource doorCreakSFX;
+    public AudioSource doorKillSFX;
 
     void Update(){
         if (Vector3.Distance(EndlessRoomManager.Instance.player.transform.position, transform.position) < playerDistToDoorTriggerKill) {
-            GetComponent<AudioSource>().Play();
+            if (doorKillSFX != null) doorKillSFX.Play();
+            if (doorAnimator != null) doorAnimator.Play(doorOpenAnimationName);
         } else if (Vector3.Distance(EndlessRoomManager.Instance.player.transform.position, transform.position) < playerDistToDoorTriggerCreak) {
-            GetComponent<AudioSource>().Play();
+            if (doorCreakSFX != null) doorCreakSFX.Play();
+            if (doorAnimator != null) doorAnimator.Play(doorCreakAnimationName);
         }
     }
 }
